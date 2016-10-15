8fc7279aca5e901adff8dc3135b76fba673df65b
RainerRoss <rainer_ross@web.de>
2016-10-15 16:32:10 +0200

    Update webservice.js

--- webservice.js
+++ webservice.js
@@ -1,8 +1,8 @@
-var http		= require('http');
-var url			= require('url');
+var http	= require('http');
+var url		= require('url');
 var querystring	= require('querystring');
-var db			= require('/QOpenSys/QIBM/ProdData/OPS/Node4/os400/db2i/lib/db2');
-var util		= require('util');
+var db		= require('/QOpenSys/QIBM/ProdData/OPS/Node4/os400/db2i/lib/db2');
+var util	= require('util');
 
 var responseData = {
     success: '',
@@ -17,50 +17,50 @@ http.createServer(function (request, response) {
     var values = querystring.parse(url.parse(request.url).query);
 	
     console.log('Pathname: ' + pathname);
-	console.log('Querystring: ' + url.parse(request.url).query);	
-	console.log('Values:' + JSON.stringify(values));
+    console.log('Querystring: ' + url.parse(request.url).query);	
+    console.log('Values:' + JSON.stringify(values));
 
-	response.writeHead(200, {'Content-Type': 'application/json'});
-	response.end(readCustomers(values.name));
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.end(readCustomers(values.name));
 	
-	console.log('Runtime: ' + timerEnd(start));
+    console.log('Runtime: ' + timerEnd(start));
 }).listen(8080);
 
 function readCustomers(value) {
-	var select = 'SELECT CUSNUM, trim(LSTNAM) as lstnam, trim(CITY) as City, BALDUE FROM QCUSTCDT';
-	var where  = util.format("where lstnam like '%s'", value);
-	var sql    = select + ' ' + where;
-	var data   = '';
-	console.log("Sql: " + sql);
-	db.init();
-	db.conn("*LOCAL");
-	db.exec("SET SCHEMA QIWS");
-	db.exec(sql, function(rows) {
-		data = createResponse(rows);
-	});
-	db.close;
-	return data;
+    var select = 'SELECT CUSNUM, trim(LSTNAM) as lstnam, trim(CITY) as City, BALDUE FROM QCUSTCDT';
+    var where  = util.format("where lstnam like '%s'", value);
+    var sql    = select + ' ' + where;
+    var data   = '';
+    console.log("Sql: " + sql);
+    db.init();
+    db.conn("*LOCAL");
+    db.exec("SET SCHEMA QIWS");
+    db.exec(sql, function(rows) {
+			data = createResponse(rows);
+    });
+    db.close;
+    return data;
 }
 
 function createResponse(rows) {
-	if (rows.length > 0) {
-		responseData.success = true;
-		responseData.errmsg  = '';
-		responseData.records = rows.length;
-		responseData.items   = rows;
-	} else {
-		responseData.success = false;
-		responseData.errmsg  = 'No records found';
-		responseData.records = rows.length;
-		responseData.items   = '';
-	}
-	return JSON.stringify(responseData);
+    if (rows.length > 0) {
+	responseData.success = true;
+	responseData.errmsg  = '';
+	responseData.records = rows.length;
+	responseData.items   = rows;
+    } else {
+	responseData.success = false;
+	responseData.errmsg  = 'No records found';
+	responseData.records = rows.length;
+	responseData.items   = '';
+    }
+    return JSON.stringify(responseData);
 }
 
 function timerStart() {
-	return (new Date()).getTime();
+    return (new Date()).getTime();
 }
 
 function timerEnd(start) {
-	return ((new Date()).getTime() - start) + "ms";
+    return ((new Date()).getTime() - start) + "ms";
 }
